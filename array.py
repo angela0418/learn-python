fcc4537abea2f42db82b5df7efc62f8f04f43b0b
angela0418 <angelaxie0418@gmail.com>
2022-01-09 11:12:46 -0500

    First init

---
+++ array.py
@@ -0,0 +1,2 @@
+class_peters = ["Angela", "X", "Y", "Z"]
+print("There are " + str(len(class_peters)) + " students!")
---
+++ hello.py
@@ -0,0 +1,5 @@
+import sys
+print(sys.argv)
+print('hello ' + sys.argv[1] + ", " + sys.argv[2]+",  " + sys.argv[3] +" and " + 
+ sys.argv[4] + "!")
+print('whats up!')
